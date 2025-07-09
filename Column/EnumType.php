3ec00a0625ed5969919e62ee4af4f5b9b3820143
Niklas de Vries <n.devries@cwd.at>
2025-07-09 13:31:26 +0200

    Fixed EnumType

--- Column/EnumType.php
+++ Column/EnumType.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace App\Infrastructure\Ui\Admin\Grid\Column;
+namespace Cwd\GridBundle\Column;
 
 use Cwd\GridBundle\Column\AbstractColumn;
 use Symfony\Component\OptionsResolver\OptionsResolver;
